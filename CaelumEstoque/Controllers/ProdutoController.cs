30b55f8e9a426d0df4941a42a7f1aab93759ad5f
amanda oliveira <amandaoliveiravianna@gmail.com>
2018-08-02 14:47:04 -0300

    commit inicial

---
+++ CaelumEstoque/Controllers/ProdutoController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaelumEstoque.DAO;
+using CaelumEstoque.Filtros;
+using CaelumEstoque.Models;
+
+
+namespace CaelumEstoque.Controllers
+{
+    [AutorizacaoFilter]
+    public class ProdutoController : Controller
+    {
+
+        // mandar lista de produto para a camada de viualização para montar uma tabela usa a propriedade viewBag
+        // GET: Produto
+        [Route("produtos", Name = "ListaProdutos")]
+        public ActionResult Index()
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            IList<Produto> produtos = dao.Lista();
+            return View(produtos);
+        }
+
+        public ActionResult Form()
+        {
+            CategoriasDAO categoriasDAO = new CategoriasDAO();
+            IList<CategoriaDoProduto> categorias = categoriasDAO.Lista();
+            ViewBag.Categorias = categorias;
+            ViewBag.Produto = new Produto();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Adiciona(Produto produto)
+        {
+            //IsValid Mostra se o modelo obedece ou nao as regras de validações
+
+            int idDaInformatica = 1;
+            if (produto.CategoriaId.Equals(idDaInformatica) && produto.Preco < 100)
+            {
+                ModelState.AddModelError("produto.Invalido", "Informatica com preco abaixo de 100 reais");
+            }
+
+            if (ModelState.IsValid)
+            {
+                ProdutosDAO dao = new ProdutosDAO();
+                dao.Adiciona(produto);
+
+                return RedirectToAction("Index", "Produto");
+            }
+            else
+            {
+                ViewBag.Produto = produto;
+                CategoriasDAO categoriasDAO = new CategoriasDAO();
+                ViewBag.Categorias = categoriasDAO.Lista();
+                return View("Form");
+            }
+        }
+
+        //pagina de visualização do detalhe do produto
+
+        [Route("produtos/{id}", Name = "VisualizaProduto")]
+        public ActionResult Visualiza(int id)
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            //buscar o produto
+            Produto produto = dao.BuscaPorId(id);
+            // enviar o produto para a camada de visualização , usa-se ViewBag
+            ViewBag.Produto = produto;
+            return View();
+        }
+
+        public ActionResult DecrementaQtd(int id)
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            Produto produto = dao.BuscaPorId(id);
+            produto.Quantidade--;
+            dao.Atualiza(produto);
+            return Json(produto);
+        }
+
+    }
+}
